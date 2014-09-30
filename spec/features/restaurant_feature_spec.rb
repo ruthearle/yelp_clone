52d737c72ad6310458d5196a213d76490a09ace9
ruthearle <rrearle@gmail.com>
2014-09-30 23:38:39 +0100

    test passed for displaying a restaurant once added to db.

--- spec/features/restaurant_feature_spec.rb
+++ spec/features/restaurant_feature_spec.rb
@@ -1,22 +1,35 @@
 require 'rails_helper'
 
 describe 'restaurants' do
-  context 'can be added' do
-    it 'by a person being prompted to add a restuarant' do
+  context 'have not been added' do
+    it 'a person is prompted to add a restuarant' do
       visit restaurants_path
       expect(page).to have_content 'No restaurants yet'
-      expect(page).to have_content 'Add a restaurant'
+      expect(page).to have_link 'Add a restaurant'
     end
   end
 
-  context 'can be displayed' do
+  context 'has already been added' do
     before do
       Restaurant.create(name: 'KFC')
     end
-    it 'after details have been added' do
-      visit restaurant_path
+    it 'should display the restaurants' do
+      visit restaurants_path
       expect(page).to have_content 'KFC'
       expect(page).not_to have_content 'No restaurants yet'
     end
   end
+
+  #context 'can be added' do
+    #it 'by a person completing a form' do
+      #visit restaurant_path
+      #click_link 'Add a restaurant'
+      #fill_in 'Name', :with 'KFC'
+      #fill_in 'Description' :with, 'Chicken and fries'
+      #click_button 'Create restaurant'
+      #expect(current_path).to eq restaurant_path
+      #expect(page).to have_content 'KFC'
+      #expect(page).not_to have_content 'No restaurants yet'
+    #end
+  #end
 end
