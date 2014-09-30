c738341adf9c216c6163ec2a66c4fa54238a3ae0
ruthearle <rrearle@gmail.com>
2014-09-30 23:45:06 +0100

    failing test written for a person creating a restaurant

--- spec/features/restaurant_feature_spec.rb
+++ spec/features/restaurant_feature_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe 'restaurants' do
   context 'have not been added' do
-    it 'a person is prompted to add a restuarant' do
+    it 'a person is prompted to add a restaurant' do
       visit restaurants_path
       expect(page).to have_content 'No restaurants yet'
       expect(page).to have_link 'Add a restaurant'
@@ -20,16 +20,16 @@ describe 'restaurants' do
     end
   end
 
-  #context 'can be added' do
-    #it 'by a person completing a form' do
-      #visit restaurant_path
-      #click_link 'Add a restaurant'
-      #fill_in 'Name', :with 'KFC'
-      #fill_in 'Description' :with, 'Chicken and fries'
-      #click_button 'Create restaurant'
-      #expect(current_path).to eq restaurant_path
-      #expect(page).to have_content 'KFC'
-      #expect(page).not_to have_content 'No restaurants yet'
-    #end
-  #end
+  describe 'creating a restaurant' do
+    it 'by a person completing a form' do
+      visit restaurant_path
+      click_link 'Add a restaurant'
+      fill_in 'Name', with: 'KFC'
+      fill_in 'Description', with: 'Chicken and fries'
+      click_button 'Create restaurant'
+      expect(current_path).to eq restaurant_path
+      expect(page).to have_content 'KFC'
+      expect(page).not_to have_content 'No restaurants yet'
+    end
+  end
 end
