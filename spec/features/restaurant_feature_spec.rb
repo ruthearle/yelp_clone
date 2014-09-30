3363219b85b6d71e8c3e50b6d0f9dec267a3e80f
ruthearle <rrearle@gmail.com>
2014-09-30 23:07:46 +0100

    failing test written for displaying a restaurant after it being added to the db.

--- spec/features/restaurant_feature_spec.rb
+++ spec/features/restaurant_feature_spec.rb
@@ -2,10 +2,21 @@ require 'rails_helper'
 
 describe 'restaurants' do
   context 'can be added' do
-    it 'by a user being prompted to add a restuarant' do
-      visit '/restaurants'
+    it 'by a person being prompted to add a restuarant' do
+      visit restaurants_path
       expect(page).to have_content 'No restaurants yet'
       expect(page).to have_content 'Add a restaurant'
+    end
+  end
+
+  context 'can be displayed' do
+    before do
+      Restaurant.create(name: 'KFC')
+    end
+    it 'after details have been added' do
+      visit restaurant_path
+      expect(page).to have_content 'KFC'
+      expect(page).not_to have_content 'No restaurants yet'
     end
   end
 end
