c86292ae17144a2794dc216c0a03044cfafb29ff
ruthearle <rrearle@gmail.com>
2014-10-02 12:25:18 +0100

    review validity tests passed - rating cannot be more than 5

--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -1,8 +1,14 @@
 require 'rails_helper'
 
 describe Review, type: :model do
-  it 'is not valid with arating of less than 1' do
+  it 'is not valid with a rating of less than 1' do
     review = Review.new(rating: -1)
+    expect{ review.valid? }.to change{ review.errors[:rating] }.to include('is not included in the list')
+    expect(review).not_to be_valid
+  end
+
+  it 'is not valid with a rating of more than 5' do
+    review = Review.new(rating: 6)
     expect{ review.valid? }.to change{ review.errors[:rating] }.to include('is not included in the list')
     expect(review).not_to be_valid
   end
